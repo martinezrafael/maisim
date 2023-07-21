0d88216218daeea76d789259e75159c28b13b884
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-07-21 08:53:01 -0300

    tabelas separadas por setor

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -17,25 +17,41 @@ const Top = ({ userCep }) => {
     fetchData();
   }, [userCep]);
 
+  const groupedData = data.reduce((acc, item) => {
+    const { SETOR_NEC_ABERTO } = item;
+    if (!acc[SETOR_NEC_ABERTO]) {
+      acc[SETOR_NEC_ABERTO] = [];
+    }
+    acc[SETOR_NEC_ABERTO].push(item);
+    return acc;
+  }, {});
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Laboratório</th>
-          <th>Share</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index} className={`item-${(index += 1)}`}>
-            <td>{item.PRODUTO}</td>
-            <td>{item.LABORATORIO}</td>
-            <td>{item.UNIDADES}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      {Object.keys(groupedData).map((setor) => (
+        <div key={setor}>
+          <h2>{setor}</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Laboratório</th>
+                <th>Share</th>
+              </tr>
+            </thead>
+            <tbody>
+              {groupedData[setor].map((item, index) => (
+                <tr key={index} className={`item-${(index += 1)}`}>
+                  <td>{item.PRODUTO}</td>
+                  <td>{item.LABORATORIO}</td>
+                  <td>{item.UNIDADES}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
+    </div>
   );
 };
 
