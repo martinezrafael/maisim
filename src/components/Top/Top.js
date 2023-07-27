b8f4f8090eea767c50192bccdf946b0a6af19a3b
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2023-07-27 11:26:00 -0300

    ajustei a tabela no desktop

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { dataIqvia } from "../../api/iqvia.api";
 import Cadeado from "../../images/icons/cadeado.svg";
-import Sacola from "../../images/icons/sacola.svg";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -55,59 +54,8 @@ const SubTitle = styled.p`
   text-align: center;
 `;
 
-//Descrição da seção
-// const DescriptionWrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   max-width: 400px;
-//   margin: auto;
-//   padding: 40px 0;
-
-//   @media screen and (max-width: 768px) {
-//     width: 100%;
-//     margin-bottom: 20px;
-//     padding: 0;
-//   }
-// `;
-
-// const DescriptionIcon = styled.img`
-//   width: 100px;
-//   margin-right: 40px;
-
-//   @media screen and (max-width: 768px) {
-//     margin-right: 10px;
-//     width: 60px;
-//   }
-// `;
-
-// const DescriptionTitle = styled.h3`
-//   color: #6f3789;
-//   font-size: 32px;
-//   font-weight: 700;
-//   margin: 0;
-
-//   @media screen and (max-width: 768px) {
-//     font-size: 24px;
-//     text-align: center;
-//     margin-bottom: 8px;
-//   }
-// `;
-
-// const DescriptionParagraph = styled.p`
-//   color: #1f002a;
-//   font-size: 18px;
-//   margin: 0;
-
-//   @media screen and (max-width: 768px) {
-//     text-align: center;
-//   }
-// `;
-
 //Tabelas
-const TableWrapper = styled.div`
+const Wrapper = styled.div`
   background: radial-gradient(
     106.63% 107.48% at 0% 0%,
     #fc46c2 0%,
@@ -116,12 +64,7 @@ const TableWrapper = styled.div`
   border-radius: 20px;
   color: #fff;
   margin-bottom: 20px;
-  padding: 60px;
-
-  @media screen and (max-width: 768px) {
-    padding: 0;
-    width: 100%;
-  }
+  padding: 40px;
 `;
 
 const TableTitle = styled.h2`
@@ -129,70 +72,50 @@ const TableTitle = styled.h2`
   margin: 0;
   margin-bottom: 20px;
   text-align: center;
-
-  @media screen and (max-width: 768px) {
-    font-size: 24px;
-    margin-bottom: 10px;
-  }
 `;
 
-const TableTitleMobile = styled.span`
-  display: none;
-
-  @media screen and (max-width: 500px) {
-    display: block;
-    margin-bottom: 4px;
-  }
+const ColumnWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 `;
 
-const TableHead = styled.th`
-  font-size: 24px;
-  font-weight: 700;
-  padding: 12px 0;
-  width: 100%;
-  @media screen and (max-width: 768px) {
-    display: none;
-    width: 100%;
-  }
-`;
-
-const TableRow = styled.tr`
-  background: #1f002a;
-  border-radius: 20px;
+const ColumTitle = styled.h3`
   text-align: center;
-  width: 100%;
 `;
 
-const TableData = styled.td`
-  padding: 20px;
-  border-radius: 10px;
-  border: none;
-  font-weight: 700;
-  font-size: 18px;
-  width: 100%;
+const RowsWrapper = styled.div`
+  align-items: center;
+  background: #1f002a;
+  border-radius: 22px;
+  display: grid;
+  margin-bottom: 12px;
+  padding: 24px 12px;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
 `;
 
-const TableButton = styled.button`
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 900;
+  justify-content: center;
+  text-align: center;
+`;
+
+const RowBtn = styled.button`
   background: radial-gradient(
     106.63% 107.48% at 0% 0%,
     #fc46c2 0%,
     #8d4aab 100%
   );
-  border-radius: 12px;
-  border: none;
-  cursor: pointer;
   color: #fff;
-  font-size: 16px;
+  border-radius: 22px;
+  border: none;
   display: flex;
   align-items: center;
-  gap: 8px;
-  padding: 10px;
-  width: 100%;
-
-  @media screen and (max-width: 500px) {
-    margin-bottom: 10px;
-    width: 90%;
-  }
+  gap: 4px;
+  padding: 12px 24px;
+  cursor: pointer;
 `;
 
 const Top = ({ onCepChange }) => {
@@ -279,18 +202,6 @@ const Top = ({ onCepChange }) => {
           negócio
         </SubTitle>
       </BoxTitle>
-      {/* <DescriptionWrapper>
-        <div>
-          <DescriptionIcon src={Sacola} alt="Sacola" />
-        </div>
-        <div>
-          <DescriptionTitle>Visão do Mercado</DescriptionTitle>
-          <DescriptionParagraph>
-            Entenda quais os itens que se destacam na sua região e aplique no
-            seu negócio
-          </DescriptionParagraph>
-        </div>
-      </DescriptionWrapper> */}
       <div>
         <CepLabel htmlFor="userCep">
           Insira seu CEP para ter acesso aos itens que mais vendem na sua
@@ -306,84 +217,58 @@ const Top = ({ onCepChange }) => {
       </div>
       <Container>
         {Object.keys(groupedData).map((setor) => {
-          const { setorTotalQuantity, items } = groupedData[setor];
+          const { items } = groupedData[setor];
           return (
-            <TableWrapper key={setor}>
+            <Wrapper key={setor}>
               <TableTitle>{formatSetor(setor)}</TableTitle>
-              <table>
-                <thead>
-                  <tr>
-                    <TableHead>Nome</TableHead>
-                    <TableHead>Laboratório</TableHead>
-                    <TableHead>Share</TableHead>
-                  </tr>
-                </thead>
-                <tbody>
-                  {items.map((item, index) => {
-                    const isShowButton = index < 3;
-                    const isItemSelected = selectedItems.includes(item);
-                    return (
-                      <React.Fragment key={index}>
-                        <TableRow className={`item-${(index += 1)}`}>
-                          <TableData>
-                            {isShowButton && !isItemSelected ? (
-                              <TableButton onClick={() => handlePurchase(item)}>
-                                <img
-                                  src={Cadeado}
-                                  alt="Desbloquear comparação"
-                                />
-                                Desbloquear comparação
-                              </TableButton>
-                            ) : (
-                              <>
-                                <TableTitleMobile>
-                                  Laboratório:
-                                </TableTitleMobile>
-                                {item.PRODUTO}
-                              </>
-                            )}
-                          </TableData>
-                          <TableData>
-                            {isShowButton && !isItemSelected ? (
-                              <TableButton onClick={() => handlePurchase(item)}>
-                                <img
-                                  src={Cadeado}
-                                  alt="Desbloquear comparação"
-                                />
-                                Desbloquear comparação
-                              </TableButton>
-                            ) : (
-                              <>
-                                <TableTitleMobile>
-                                  Laboratório:
-                                </TableTitleMobile>
-                                {item.LABORATORIO}
-                              </>
-                            )}
-                          </TableData>
-                          <TableData>
-                            {isShowButton && !isItemSelected ? (
-                              <TableButton onClick={() => handlePurchase(item)}>
-                                <img
-                                  src={Cadeado}
-                                  alt="Desbloquear comparação"
-                                />
-                                Desbloquear comparação
-                              </TableButton>
-                            ) : (
-                              <>
-                                <TableTitleMobile>Share:</TableTitleMobile>
-                                {item.REPRESENTACAO}%
-                              </>
-                            )}
-                          </TableData>
-                        </TableRow>
-                      </React.Fragment>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </TableWrapper>
+              <ColumnWrapper>
+                <ColumTitle>Nome</ColumTitle>
+                <ColumTitle>Laboratório</ColumTitle>
+                <ColumTitle>Share</ColumTitle>
+              </ColumnWrapper>
+              <div>
+                {items.map((item, index) => {
+                  const isShowButton = index < 3;
+                  const isItemSelected = selectedItems.includes(item);
+                  return (
+                    <React.Fragment key={index}>
+                      <RowsWrapper className={`item-${(index += 1)}`}>
+                        <Row>
+                          {isShowButton && !isItemSelected ? (
+                            <RowBtn onClick={() => handlePurchase(item)}>
+                              <img src={Cadeado} alt="Desbloquear comparação" />
+                              Desbloquear comparação
+                            </RowBtn>
+                          ) : (
+                            <>{item.PRODUTO}</>
+                          )}
+                        </Row>
+                        <Row>
+                          {isShowButton && !isItemSelected ? (
+                            <RowBtn onClick={() => handlePurchase(item)}>
+                              <img src={Cadeado} alt="Desbloquear comparação" />
+                              Desbloquear comparação
+                            </RowBtn>
+                          ) : (
+                            <>{item.LABORATORIO}</>
+                          )}
+                        </Row>
+                        <Row>
+                          {isShowButton && !isItemSelected ? (
+                            <RowBtn onClick={() => handlePurchase(item)}>
+                              <img src={Cadeado} alt="Desbloquear comparação" />
+                              Desbloquear comparação
+                            </RowBtn>
+                          ) : (
+                            <>{item.REPRESENTACAO}%</>
+                          )}
+                        </Row>
+                      </RowsWrapper>
+                    </React.Fragment>
+                  );
+                })}
+              </div>
+            </Wrapper>
           );
         })}
       </Container>
